946c9752506d232d02d588e5cd2aaee4c4bde580
Mankuy <facajgscomplex@gmail.com>
2026-03-25 10:30:46 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDFCba95ny7I2HAA2KVm8IQqzqg-YkLJDo",
+  authDomain: "registro-gastos-8a864.firebaseapp.com",
+  projectId: "registro-gastos-8a864",
+  storageBucket: "registro-gastos-8a864.firebasestorage.app",
+  messagingSenderId: "893257009763",
+  appId: "1:893257009763:web:56d9c9dbdb682a3417f576",
+  measurementId: "G-P81R3MJQP7"
+};
+const FB_COLLECTIONS = {
+  users:    'users',
+  profiles: 'profiles'
+};
+const FB_DOCS = {
+  userAuth: 'auth'
+};
