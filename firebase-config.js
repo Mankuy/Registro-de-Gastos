6ee9511828426076e13974d40c0f8ddda5345473
Mankuy <facajgscomplex@gmail.com>
2026-03-28 14:02:04 -0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,17 @@
 const firebaseConfig = {
-  apiKey: 'AIzaSyDFCba95ny7I2HAA2KVm8IQgzgq-YkLJ0o',
-  authDomain: 'registro-gastos-8a864.firebaseapp.com',
-  projectId: 'registro-gastos-8a864',
-  storageBucket: 'registro-gastos-8a864.firebasestorage.app',
-  messagingSenderId: '893257009763',
-  appId: '1:893257009763:web:56d9c9dbdb682a3417f576'
+  apiKey: "AIzaSyDFCba95ny7I2HAA2KVm8IQqzqg-YkLJDo",
+  authDomain: "registro-gastos-8a864.firebaseapp.com",
+  projectId: "registro-gastos-8a864",
+  storageBucket: "registro-gastos-8a864.firebasestorage.app",
+  messagingSenderId: "893257009763",
+  appId: "1:893257009763:web:56d9c9dbdb682a3417f576",
+  databaseURL: "https://registro-gastos-8a864-default-rtdb.firebaseio.com/"
 };
 
-firebase.initializeApp(firebaseConfig);
-const db     = firebase.firestore();
-const fbAuth = firebase.auth();
+const FB_COLLECTIONS = {
+  users:    'users',
+  profiles: 'profiles'
+};
+const FB_DOCS = {
+  userAuth: 'auth'
+};
